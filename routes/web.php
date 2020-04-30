bccbfa913b15a4c6c4e70a537f1f00a9fef5a906
Jonathan Marshall <Jonathan@marshalltech.co.uk>
2020-04-30 11:48:11 +0100

    Removed redundant middleware usage as the cors middleware is set globally.

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ use Dusterio\LumenPassport\LumenPassport;
 $router->options(
     '/{any:.*}',
     [
-        'middleware' => ['cors'],
         function (){
             return response(['success' => true])
                 ->header('Access-Control-Allow-Origin', '*', true)
@@ -33,65 +32,63 @@ $router->options(
     ]
 );
 
-$router->group(['middleware' => 'cors'], function() use ($router) {
-    // App root.
+// App root.
+$router->get('/', function (Request $request) use ($router) {
+    return response()->json([
+        'version' => $router->app->version(),
+    ]);
+});
+
+// Oauth/Passport Register, Login, Logout routes.
+$router->group(['prefix' => 'v1/oauth'], function () use ($router) {
+    LumenPassport::routes($router, ['prefix' => '']);
+    $router->post('register', ['uses' => 'AuthController@register']);
+    $router->post('logout', ['middleware' => 'auth', 'uses' => 'AuthController@logout']);
+});
+
+// Main application routes.
+// User authentication required.
+$router->group(['middleware' => 'auth', 'prefix' => 'v1'], function () use ($router) {
+    // Lumen version, current logged in user info.
     $router->get('/', function (Request $request) use ($router) {
         return response()->json([
             'version' => $router->app->version(),
+            'user' => User::currentUser(),
         ]);
     });
 
-    // Oauth/Passport Register, Login, Logout routes.
-    $router->group(['prefix' => 'v1/oauth'], function () use ($router) {
-        LumenPassport::routes($router, ['prefix' => '']);
-        $router->post('register', ['uses' => 'AuthController@register']);
-        $router->post('logout', ['middleware' => 'auth', 'uses' => 'AuthController@logout']);
+    // Endpoints for User Profile.
+    $router->group(['prefix' => 'profile'], function () use ($router) {
+        $router->get('/', ['uses' => 'ProfileController@index']);
+        $router->post('update', ['uses' => 'ProfileController@update']);
     });
 
-    // Main application routes.
-    // User authentication required.
-    $router->group(['middleware' => 'auth', 'prefix' => 'v1'], function () use ($router) {
-        // Lumen version, current logged in user info.
-        $router->get('/', function (Request $request) use ($router) {
-            return response()->json([
-                'version' => $router->app->version(),
-                'user' => User::currentUser(),
-            ]);
-        });
+    // Endpoints for Users.
+    $router->group(['prefix' => 'users', 'middleware' => SuperUserMiddleware::class], function () use ($router) {
+        $router->get('/', ['uses' => 'UserController@index']);
+        $router->post('create', ['uses' => 'UserController@create']);
+        $router->get('{id}/view', ['uses' => 'UserController@view']);
+        $router->post('{id}/update', ['uses' => 'UserController@update']);
+        $router->post('{id}/delete', ['uses' => 'UserController@delete']);
+    });
 
-        // Endpoints for User Profile.
-        $router->group(['prefix' => 'profile'], function () use ($router) {
-            $router->get('/', ['uses' => 'ProfileController@index']);
-            $router->post('update', ['uses' => 'ProfileController@update']);
-        });
+    // Endpoints for Folders.
+    $router->group(['prefix' => 'folders'], function () use ($router) {
+        $router->get('/', ['uses' => 'FolderController@index']);
+        $router->post('create', ['uses' => 'FolderController@create']);
+        $router->get('{id}/view', ['uses' => 'FolderController@view']);
+        $router->post('{id}/update', ['uses' => 'FolderController@update']);
+        $router->post('{id}/delete', ['uses' => 'FolderController@delete']);
+    });
 
-        // Endpoints for Users.
-        $router->group(['prefix' => 'users', 'middleware' => SuperUserMiddleware::class], function () use ($router) {
-            $router->get('/', ['uses' => 'UserController@index']);
-            $router->post('create', ['uses' => 'UserController@create']);
-            $router->get('{id}/view', ['uses' => 'UserController@view']);
-            $router->post('{id}/update', ['uses' => 'UserController@update']);
-            $router->post('{id}/delete', ['uses' => 'UserController@delete']);
-        });
+    // Endpoints for Files in a Folder.
+    $router->group(['prefix' => 'files'], function () use ($router) {
+        $router->get('/', ['uses' => 'FileController@index']);
+        $router->post('create', ['uses' => 'FileController@create']);
+        $router->get('{id}/view', ['uses' => 'FileController@view']);
+        $router->post('{id}/update', ['uses' => 'FileController@update']);
+        $router->post('{id}/delete', ['uses' => 'FileController@delete']);
 
-        // Endpoints for Folders.
-        $router->group(['prefix' => 'folders'], function () use ($router) {
-            $router->get('/', ['uses' => 'FolderController@index']);
-            $router->post('create', ['uses' => 'FolderController@create']);
-            $router->get('{id}/view', ['uses' => 'FolderController@view']);
-            $router->post('{id}/update', ['uses' => 'FolderController@update']);
-            $router->post('{id}/delete', ['uses' => 'FolderController@delete']);
-        });
-
-        // Endpoints for Files in a Folder.
-        $router->group(['prefix' => 'files'], function () use ($router) {
-            $router->get('/', ['uses' => 'FileController@index']);
-            $router->post('create', ['uses' => 'FileController@create']);
-            $router->get('{id}/view', ['uses' => 'FileController@view']);
-            $router->post('{id}/update', ['uses' => 'FileController@update']);
-            $router->post('{id}/delete', ['uses' => 'FileController@delete']);
-
-            $router->get('{id}/download', ['uses' => 'FileController@download']);
-        });
+        $router->get('{id}/download', ['uses' => 'FileController@download']);
     });
 });
